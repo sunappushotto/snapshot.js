0405bc18a1eec9bc80c3a2ee4cf4aae03f7a5256
Kasumi <kasumi.null@yandex.com>
2022-03-22 13:34:26 -0500

    use LNS on smartBCH

--- src/utils/provider.ts
+++ src/utils/provider.ts
@@ -8,13 +8,24 @@ const providers = {};
 
 export default function getProvider(network: string) {
   const url: any = networks[network].rpc[0];
-  const connectionInfo = typeof url === 'object' ? {...url, timeout: 25000} : {url, timeout: 25000};
+  const connectionInfo = typeof url === 'object' ? {
+    ...url,
+    timeout: 25000,
+    ensAddress: '0xCfb86556760d03942EBf1ba88a9870e67D77b627',
+  } : {
+    url,
+    timeout: 25000,
+    ensAddress: '0xCfb86556760d03942EBf1ba88a9870e67D77b627',
+  };
   if (!providers[network]) providers[network] = new StaticJsonRpcProvider(connectionInfo);
   return providers[network];
 }
 
 export function getBatchedProvider(network: string) {
   const url: string = networks[network].rpc[0];
-  if (!providers[network]) providers[network] = new JsonRpcBatchProvider(url);
+  if (!providers[network]) providers[network] = new JsonRpcBatchProvider({
+    url,
+    ensAddress: '0xCfb86556760d03942EBf1ba88a9870e67D77b627',
+  });
   return providers[network];
 }
