87f781f8bb3df4660ff404dbc176417f3cddac56
Kasumi <kasumi.null@yandex.com>
2022-03-22 14:23:40 -0500

    refactor

--- src/utils/provider.ts
+++ src/utils/provider.ts
@@ -6,16 +6,21 @@ import networks from '../networks.json';
 
 const providers = {};
 
+const timeout = 25000;
+const ensAddress = '0xCfb86556760d03942EBf1ba88a9870e67D77b627';
+const ensResolver = '0x1Ba19b976feFC1C9c684F2B821E494A380f45A0f';
+
 export default function getProvider(network: string) {
   const url: any = networks[network].rpc[0];
   const connectionInfo = typeof url === 'object' ? {
     ...url,
-    timeout: 25000,
-    ensAddress: '0xCfb86556760d03942EBf1ba88a9870e67D77b627',
+    timeout,
+    ensAddress,
+    ensResolver,
   } : {
     url,
-    timeout: 25000,
-    ensAddress: '0xCfb86556760d03942EBf1ba88a9870e67D77b627',
+    timeout,
+    ensAddress,
   };
   if (!providers[network]) providers[network] = new StaticJsonRpcProvider(connectionInfo);
   return providers[network];
@@ -25,7 +30,7 @@ export function getBatchedProvider(network: string) {
   const url: string = networks[network].rpc[0];
   if (!providers[network]) providers[network] = new JsonRpcBatchProvider({
     url,
-    ensAddress: '0xCfb86556760d03942EBf1ba88a9870e67D77b627',
+    ensAddress,
   });
   return providers[network];
 }
